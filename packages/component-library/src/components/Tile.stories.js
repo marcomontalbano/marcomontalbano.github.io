e778ef4ffac735d57e3949dce38a7abe437202da
Marco Montalbano <marcomontalbano.work@gmail.com>
2019-09-17 15:38:51 +0200

    Truncate text on tile after 3 lines

--- packages/component-library/src/components/Tile.stories.js
+++ packages/component-library/src/components/Tile.stories.js
@@ -8,13 +8,13 @@ import Tile from './Tile';
 export const createTile = () => ({
     title: 'This is the title',
     description: 'This is the description for this tile',
-    src: `https://picsum.photos/1200/580?random=${Math.random()}`,
+    src: `https://picsum.photos/seed/picsum/1200/580`,
     href: `https://example.com`
 });
 
 export const tileWithLongDescription = {
     ...createTile(),
-    description: `This is the description for this tile with a very long description. Is it enough? I don't think so, lorem ipsum.`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non posuere justo, a imperdiet leo. Etiam non ipsum a felis lobortis convallis sit amet id odio. Aliquam interdum posuere enim. Donec sollicitudin interdum sodales. Pellentesque eget nibh ac massa tristique eleifend in vitae lorem. Donec semper lectus et fermentum vehicula. Ut elementum fermentum nulla. Ut eget cursus nisi. Aliquam nec faucibus dolor. Pellentesque felis velit, volutpat eget egestas nec, ultricies eget magna. Proin iaculis consequat ante.`,
 };
 
 storiesOf('Tile', module)
