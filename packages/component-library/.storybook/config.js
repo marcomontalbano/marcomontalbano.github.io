4f3f578400d7e34d5fa7ddedea83f6e3c60bd888
Marco Montalbano <marcomontalbano.work@gmail.com>
2020-03-05 01:17:59 +0100

    Remove WARN when building

--- packages/component-library/.storybook/config.js
+++
@@ -1,7 +0,0 @@
-import { configure } from '@storybook/react';
-import requireContext from 'require-context.macro';
-
-import '../src/storybook.css';
-
-// automatically import all files ending in *.stories.tsx
-configure(requireContext('../src', true, /\.stories\.tsx$/), module);
---
+++ packages/component-library/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+    stories: ['../src/components/**/*.stories.tsx'],
+    addons: [
+        '@storybook/preset-create-react-app',
+        '@storybook/addon-actions',
+        '@storybook/addon-knobs',
+        '@storybook/addon-links'
+    ],
+};
