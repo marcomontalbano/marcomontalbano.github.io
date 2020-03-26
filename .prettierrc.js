ad648c274c27f4b5160c88b742119d641ee5a983
Marco Montalbano <marcomontalbano.work@gmail.com>
2020-03-26 08:51:24 +0100

    Add pre-push hook

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: 'es5',
+    useTabs: false,
+    tabWidth: 4,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    semi: false,
+    singleQuote: true,
+    printWidth: 120,
+}
