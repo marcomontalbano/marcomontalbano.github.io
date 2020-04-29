f53e14685a8ae0f54b04fb7132429c3c2c158492
Marco Montalbano <marcomontalbano.work@gmail.com>
2020-04-29 20:28:18 +0200

    Fix component order for styled-components https://git.io/JfY2R

--- packages/component-library/src/components/Page.tsx
+++ packages/component-library/src/components/Page.tsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components'
 
 const Page = styled.div`
+    --color-text: #223a48;
+    --color-link: #559cd0;
     font-family: 'Lato', sans-serif;
+    color: var(--color-text);
+
+    a {
+        color: var(--color-link);
+    }
 `
 
 export default Page
--- packages/component-library/src/index.ts
+++ packages/component-library/src/index.ts
@@ -1,7 +1,7 @@
+import Page from './components/Page'
 import Header, { Props as HeaderProps } from './components/Header'
 import Tile, { Props as TileProps } from './components/Tile'
 import TileList, { Props as TileListProps } from './components/TileList'
-import Page from './components/Page'
 import Markdown, { Props as MarkdownProps } from './components/Markdown'
 
 export { Header, Tile, TileList, Page, Markdown }
