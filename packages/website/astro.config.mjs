6c4707374154c2fb36d65b3a83490099d4e9a6e9
Marco Montalbano <marcomontalbano.work@gmail.com>
2024-04-22 23:42:51 +0200

    feat: parse mp4 video from markdown

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,11 +1,13 @@
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from 'astro/config'
+import rehypeVideo from 'rehype-video'
 import remarkGemoji from 'remark-gemoji'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   markdown: {
-    remarkPlugins: [remarkGemoji]
+    remarkPlugins: [remarkGemoji],
+    rehypePlugins: [[rehypeVideo, { details: false }]]
   }
 })
