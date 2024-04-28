0c28328429ec5407d15dccf78aa289275caaeaf8
Marco Montalbano <marcomontalbano.work@gmail.com>
2024-04-28 06:30:14 +0200

    fix: sanitize slug and description

--- packages/website/src/lib/gh-dump/run.ts
+++ packages/website/src/lib/gh-dump/run.ts
@@ -9,8 +9,8 @@ repositories.forEach((repository) => {
   if (repository.customFields.readme.ok && repository.customFields.readme.source != null) {
     const file = resolve(process.cwd(), 'src', 'content', 'project', `${repository.name}.md`)
     const data = `---
-slug: "${repository.name.replaceAll('"', '\"') }"
-description: "${repository.description?.replaceAll('"', '\"') }"
+slug: "${repository.name.replaceAll('\"', '\\"') }"
+description: "${repository.description?.replaceAll('\"', '\\"') }"
 cover: ${repository.customFields.cover.cdn}
 repoUrl: ${repository.url}
 homepageUrl: ${repository.homepageUrl}
